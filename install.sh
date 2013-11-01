ed4aade621498455ea452dc61c5fc2da782afbb4
Weston Ruter <weston@x-team.com>
2013-11-01 10:44:51 -0700

    Add initial install.sh script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $(dirname $0)
+repo_dir=$(pwd)
+
+if [ ! -e ~/Library/Preferences/WebIde* ]; then
+    echo "Could not find WebId* directorys"
+    exit
+fi
+
+webide_dir=$(ls -d ~/Library/Preferences/WebIde* | tail -n 1)
+
+if [ ! -L $webide_dir/options/project.default.xml ]; then
+    echo "Installing project.default.xml"
+    if [ -e $webide_dir/options/project.default.xml ]; then
+        mv $webide_dir/options/project.default.xml{,-backup-$(date "+%Y%m%dT%H%M%S")}
+    fi
+    cd $webide_dir/options/
+    ln -s $repo_dir/project.default.xml .
+    cd $repo_dir
+fi
+
+if [ ! -e ~/.jshintrc ]; then
+    echo "Add default ~/.jshintrc"
+    curl -L https://raw.github.com/x-team/wp-plugin-dev-lib/master/.jshintrc > ~/.jshintrc
+fi
+
+if [ ! -e $webide_dir/codestyles/WordPress.xml ]; then
+    echo "Install WordPress coding style"
+    cd $webide_dir/codestyles
+    ln -s $repo_dir/WordPress.xml .
+    cd $repo_dir
+fi
