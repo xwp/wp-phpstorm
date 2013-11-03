f4ee67b3f1adfe175e4720964875445ab404f415
Shady Sharaf <shady@sharaf.me>
2013-11-03 04:12:18 +0200

    Add support for linux, use unaliased `ls`

--- install.sh
+++ install.sh
@@ -3,12 +3,20 @@
 cd $(dirname $0)
 repo_dir=$(pwd)
 
-if [ ! -e ~/Library/Preferences/WebIde* ]; then
-    echo "Could not find WebId* directorys"
-    exit
+if [ "$(uname)" == "Darwin" ]; then
+    user_config_dir="/Library/Preferences/WebIde"    
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    user_config_dir="$HOME/.WebIde"
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    #user_config_dir="?"
 fi
 
-webide_dir=$(ls -d ~/Library/Preferences/WebIde* | tail -n 1)
+webide_dir=`$(which ls) -d $user_config_dir* | tail -n 1`
+
+if [ ! $webide_dir ]; then
+    echo "Could not find WebId* directories"
+    exit
+fi
 
 if [ ! -L $webide_dir/options/project.default.xml ]; then
     echo "Installing project.default.xml"
